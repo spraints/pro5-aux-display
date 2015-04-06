8af6c4c2e166f270d12e6cb64e2851d328c9e688
Matt Burke <spraints@gmail.com>
2015-04-06 14:52:43 -0400

    Make a struct for connect args.

--- main.go
+++ main.go
@@ -5,15 +5,31 @@ import (
   "fmt"
 )
 
+type Pro5ConnectInfo struct {
+  Host string
+  Port int
+  Password string
+}
+
 func main() {
+  connectInfo := Pro5ConnectInfo{}
+
   var listenPort = flag.Int("port", 3000, "The port that this server listens on.")
-  var remoteHost = flag.String("pro5-host", "localhost", "The name of the pro5 server.")
-  var remotePort = flag.Int("pro5-port", 9002, "The port that pro5's remote stage display is listening on.")
-  var remotePassword = flag.String("pro5-password", "stage", "The password to the pro5 remote stage display")
+  flag.StringVar(&connectInfo.Host, "pro5-host", "localhost", "The name of the pro5 server.")
+  flag.IntVar(&connectInfo.Port, "pro5-port", 9002, "The port that pro5's remote stage display is listening on.")
+  flag.StringVar(&connectInfo.Password, "pro5-password", "stage", "The password to the pro5 remote stage display")
 
   flag.Parse()
 
   fmt.Printf("listen on %d\n", *listenPort)
-  fmt.Printf("connect to %s:%d\n", *remoteHost, *remotePort)
-  fmt.Printf("password = %s\n", *remotePassword)
+
+  ConnectToPro5(connectInfo)
+}
+
+type Pro5Connection struct {
+}
+
+func ConnectToPro5(info Pro5ConnectInfo) Pro5Connection {
+  fmt.Printf("connect to %s:%d with pw=%s\n", info.Host, info.Port, info.Password)
+  return Pro5Connection{}
 }
