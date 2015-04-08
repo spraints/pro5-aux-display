a5587012baa4b9de876877e4fe94b01eb342ab1b
Matt Burke <spraints@gmail.com>
2015-04-08 18:06:31 -0400

    Build a layout from <DisplayLayouts>

--- public/app.js
+++ public/app.js
@@ -12,12 +12,97 @@ $(function() {
   ws.onclose = function() {
     console.log("CLOSE");
     $("body").removeClass("connected");
+    setMessage("closed.");
   };
   ws.onmessage = function(event) {
     console.log(event);
-    setMessage(event.data);
+
+    var xml = $.parseXML(event.data).firstChild;
+    var docType = xml.nodeName;
+    var handler = handlers[docType];
+    if (handler) {
+      new handler(xml);
+    } else {
+      console.log("unknown element type");
+      setMessage("unknown message");
+    }
+    window.xml = xml;
   };
 
+  var handlers = {};
+  handlers.DisplayLayouts = function(xml) {
+    setMessage("layout");
+    window.d = xml;
+
+    var layouts = {};
+    eachChildElement(xml, function(layoutNode) {
+      var layout = new Layout(layoutNode);
+      layouts[layout.name] = layout;
+    });
+    layouts[xml.getAttribute("selected")].arrange($("#stage-container"));
+  };
+  handlers.StageDisplayData = function(xml) {
+    setMessage("slide");
+    $("#js-slide").text(xml.outerHTML);
+    console.log("New slide");
+    console.log(xml);
+  };
+
+  function eachChildElement(xml, fn) {
+    for (var i = 0; i < xml.childNodes.length; i++) {
+      fn(xml.childNodes[i]);
+    }
+  }
+
+  function Layout(xml) {
+    this.name = xml.getAttribute("identifier");
+
+    this.arrange = function($e) {
+      $e.empty().append(build());
+    };
+
+    //var totalWidth = xml.getAttribute("width");
+    //var totalHeight = xml.getAttribute("height");
+
+    function build() {
+      var main = buildBlock(xml);
+      main.addClass("display-frame");
+      main.css("position", "relative");
+      eachChildElement(xml, function(frame) {
+        main.append(buildFrame(frame));
+      });
+      return main;
+    };
+
+    function buildFrame(node) {
+      var block = buildBlock(node);
+      var identifier = node.getAttribute("identifier");
+      block.css("position", "absolute");
+      block.css("top", coord(node, "yAxis"));
+      block.css("left", coord(node, "xAxis"));
+      block.text(identifier);
+      var label = $("<div></div>").text(identifier).css("position", "absolute").css("top", 0).css("left", 0).css("background-color", "black").css("color", "white").css("font-weight", "bold");
+      var content = $("<div></div>").css("position", "absolute").css("top", 0).css("bottom", 0).css("left", 0).css("right", 0);
+      content.addClass(classForField(node));
+      content.css("font-size", node.getAttribute("fontSize"));
+      block.append(label).append(content);
+      return block;
+    }
+
+    function buildBlock(node) {
+      var e = $("<div></div>");
+      e.width(coord(node, "width"));
+      e.height(coord(node, "height"));
+      e.css("border", "solid 1px black");
+      return e;
+    }
+
+    function coord(node, attrName) {
+      return parseInt(node.getAttribute(attrName));
+    }
+
+    return this;
+  }
 });
 // <DisplayLayouts selected="Default">
 //   <DisplayLayout showBorder="1E0" width="1680" identifier="Default" height="1050">
