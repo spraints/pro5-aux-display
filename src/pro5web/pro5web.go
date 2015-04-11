ec8e1b51e2d23537e4fd69bf8379dea618034002
Matt Burke <spraints@gmail.com>
2015-04-11 00:17:02 -0400

    Show goroutines.

--- src/pro5web/pro5web.go
+++ src/pro5web/pro5web.go
@@ -3,6 +3,7 @@ package pro5web
 import (
   "fmt"
   "net/http"
+  "runtime/pprof"
 
   "golang.org/x/net/websocket"
 )
@@ -17,6 +18,7 @@ type NewChannels chan<- (chan<- string)
 func StartServer(info WebServerInfo, messageChannels NewChannels) {
   var mux = http.NewServeMux()
   mux.Handle("/connect", serveWebSockets(messageChannels))
+  mux.HandleFunc("/_goroutines", dumpGoroutines)
   mux.Handle("/", http.FileServer(http.Dir("public/")))
   http.ListenAndServe(fmt.Sprintf(":%d", info.Port), mux)
   // This never returns.
@@ -30,4 +32,15 @@ func serveWebSockets(messageChannels NewChannels) websocket.Handler {
       fmt.Fprintf(ws, "%s", message)
     }
   }
+}
+
+func dumpGoroutines(response http.ResponseWriter, request *http.Request) {
+  response.Header().Set("Content-Type", "text/plain")
+  goroutines := pprof.Lookup("goroutine")
+  if goroutines == nil {
+    response.WriteHeader(404)
+    fmt.Fprintf(response, "Couldn't load goroutine profile.\r\n")
+    return
+  }
+  goroutines.WriteTo(response, 1)
 }
